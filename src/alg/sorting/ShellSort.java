3b95ce23d1f7827c2b7521cf50f04c9a7ad15973
Einar Snorrason <Einar.Snorrason@student.uib.no>
2017-09-14 11:44:18 +0200

    Updated some sorting algorithms

--- src/alg/sorting/ShellSort.java
+++ src/alg/sorting/ShellSort.java
@@ -1,10 +1,10 @@
 package alg.sorting;
 
 /**
- * Created by Einar Snorrason on 04/09/2017.
+ * ShellSort implementation
  */
 public class ShellSort {
-    public static void hsort(int h, Comparable[] a){
+    private static void hsort(int h, Comparable[] a){
         for (int i=h;i<a.length;i++){
             Comparable value = a[i];
             int j;
@@ -28,6 +28,7 @@ public class ShellSort {
     public static boolean less(Comparable a, Comparable b){
         return a.compareTo(b)<0;
     }
+
 
 
 
